d751d079c65f231116d21e9827832a8471b03ce6
neo <tianm_buaa@163.com>
2019-07-05 21:02:56 +0800

    update

--- tools/copy2leet.py
+++ tools/copy2leet.py
@@ -1,4 +1,5 @@
 import os, sys
+import shutil
 from shutil import copyfile
 from shutil import copytree
 
@@ -49,11 +50,38 @@ def copy2leet():
         dst_solution_java = os.path.join(dst_java_path, record)
 
         if os.path.exists(solution_cpp) and not os.path.exists(dst_solution_cpp):
-            copytree(solution_cpp, dst_solution_cpp)
+            os.makedirs(dst_solution_cpp)
+
+            for so in os.listdir(solution_cpp):
+                if (so.startswith("So")):
+                    solution = os.path.join(solution_cpp, so)
+                    dst_solution = os.path.join(dst_solution_cpp, so)
+                    copyfile(solution, dst_solution)
+
         if os.path.exists(solution_py) and not os.path.exists(dst_solution_py):
-            copytree(solution_py, dst_solution_py)
+            os.makedirs(dst_solution_py)
+
+            for so in os.listdir(solution_py):
+                if (so.startswith("So")):
+                    solution = os.path.join(solution_py, so)
+                    dst_solution = os.path.join(dst_solution_py, so)
+                    copyfile(solution, dst_solution)
+
         if os.path.exists(solution_java) and not os.path.exists(dst_solution_java):
-            copytree(solution_java, dst_solution_java)
+            os.makedirs(dst_solution_java)
+
+            for so in os.listdir(solution_java):
+                if (so.startswith("So")):
+                    solution = os.path.join(solution_java, so)
+                    dst_solution = os.path.join(dst_solution_java, so)
+                    copyfile(solution, dst_solution)
+
+        # if os.path.exists(solution_cpp) and not os.path.exists(dst_solution_cpp):
+        #     copytree(solution_cpp, dst_solution_cpp)
+        # if os.path.exists(solution_py) and not os.path.exists(dst_solution_py):
+        #     copytree(solution_py, dst_solution_py)
+        # if os.path.exists(solution_java) and not os.path.exists(dst_solution_java):
+        #     copytree(solution_java, dst_solution_java)
 
 
 if __name__ == "__main__":
