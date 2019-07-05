0a3955124dd1ab925a7afc1517b825c502465aa2
neo <tianm_buaa@163.com>
2019-07-05 14:21:21 +0800

    update

--- algorithms/python/tools/add_records.py
+++ algorithms/python/tools/add_records.py
@@ -86,10 +86,10 @@ def add_java_records():
             for solution in os.listdir(problem_python):
                 abs = os.path.join(problem_python, solution)
                 if flag:
-                    solution_python = "[Python](." + abs[22:] + ")"
+                    solution_python = "[Py](." + abs[22:] + ")"
                     solution_python = solution_python.replace("\\", "/")
                 else:
-                    solution_python = "[Python](." + abs[32:] + ")"
+                    solution_python = "[Py](." + abs[32:] + ")"
                     solution_python = solution_python.replace("\\", "/")
                 # print(solution_python)
         except Exception as e:
@@ -103,10 +103,10 @@ def add_java_records():
             for solution in os.listdir(problem_cpp):
                 abs = os.path.join(problem_cpp, solution)
                 if flag:
-                    solution_cpp = "[C++](." + abs[32:] + ")"
+                    solution_cpp = " [C++](." + abs[32:] + ")"
                     solution_cpp = solution_cpp.replace("\\", "/")
                 else:
-                    solution_cpp = "[C++](." + abs[32:] + ")"
+                    solution_cpp = " [C++](." + abs[32:] + ")"
                     solution_cpp = solution_cpp.replace("\\", "/")
         except Exception as e:
             pass
