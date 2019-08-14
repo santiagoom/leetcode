9ccdb0e774226a02130f6b27d3da7b8bc4c88160
neo <neo_buaa@163.com>
2019-08-14 22:14:10 +0800

    004

---
+++ algorithms/java/src/_004_MedianOfTwoSortedArrays/_004_MedianOfTwoSortedArrays_practise.java
@@ -0,0 +1,12 @@
+package _004_MedianOfTwoSortedArrays;
+
+public class _004_MedianOfTwoSortedArrays_practise {
+	public static void main(String[] args) {
+	    _004_MedianOfTwoSortedArrays_practise_ so = new _004_MedianOfTwoSortedArrays_practise_();
+
+	}
+}
+
+class _004_MedianOfTwoSortedArrays_practise_ {
+
+}
---
+++ algorithms/java/src/_004_MedianOfTwoSortedArrays/_004_MedianOfTwoSortedArrays_std_debug.java
@@ -0,0 +1,12 @@
+package _004_MedianOfTwoSortedArrays;
+
+public class _004_MedianOfTwoSortedArrays_std_debug {
+	public static void main(String[] args) {
+	    _004_MedianOfTwoSortedArrays_std_debug_ so = new _004_MedianOfTwoSortedArrays_std_debug_();
+
+	}
+}
+
+class _004_MedianOfTwoSortedArrays_std_debug_ {
+
+}
