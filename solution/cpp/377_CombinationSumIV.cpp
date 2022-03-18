fca7dc1841e27706b1251bf3365d59fb826946d9
santiagoom <ingenuity_zero@163.com>
2022-03-18 14:08:37 +0800

    update 337

--- solution/cpp/377_CombinationSumIV.cpp
+++ solution/cpp/377_CombinationSumIV.cpp
@@ -8,16 +8,23 @@ class Solution {
 public:
     int combinationSum4(vector<int> &nums, int target) {
         int n = nums.size();
-        unsigned long f[target + 1];
-        memset(f, 0, sizeof(f));
+//        unsigned long f[target + 1];
+        vector<int> f(target + 1, 0);
+//        memset(f, 0, sizeof(f));
 
         f[0] = 1;
         for (int i = 1; i <= target; i++) {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++) {
                 if (i - nums[j] >= 0) {
                     f[i] += f[i - nums[j]];
                 }
+                CppUtils::print_1d_vector(f);
+            }
+            CppUtils::print_1d_vector(f);
+            CppUtils::print();
         }
+
+
         return f[target];
     }
 };
